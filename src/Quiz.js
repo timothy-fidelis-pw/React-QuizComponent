2ab5a096ae027fb50115bfe1c75d1fe28f33c33e
Timothy John Fidelis <timothy.fidelis@pointwest.com.ph>
2019-02-06 19:11:19 +0800

    Module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,6 +12,12 @@ class Quiz extends Component {
     };
   }
 
+  showNextQuestion() {
+    this.setState(prevState => {
+      quiz_position: ++prevState.quiz_position;
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
@@ -23,6 +29,7 @@ class Quiz extends Component {
             <QuizEnd />
           ) : (
             <QuizQuestion
+              showNextQuestionHandler={this.showNextQuestion.bind(this)}
               quiz_question={
                 quizData.quiz_questions[this.state.quiz_position - 1]
               }
