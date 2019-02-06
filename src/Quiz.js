11688a488c0485050c260d5f1b3212c07ea5ec6d
Timothy John Fidelis <timothy.fidelis@pointwest.com.ph>
2019-02-06 18:39:07 +0800

    Module 2

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizQuestion from './QuizQuestion';
 
 let quizData = require('./quiz_data.json');
 
@@ -14,7 +15,11 @@ class Quiz extends Component {
     return (
       <div>
         <div className="QuizQuestion">
-          {quizData.quiz_questions[0].instruction_text}
+          <QuizQuestion
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
         </div>
       </div>
     );
